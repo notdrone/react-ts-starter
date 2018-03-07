265317a32e330ff1752d0854ef2490cc89d7291c
˗ˏˋdroneˎˊ˗ <notdrone@gmail.com>
2018-03-07 12:39:04 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div>
+        <p>
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import App from './App'
+
+ReactDOM.render(<App /> as any, document.getElementById('root') as Element)
